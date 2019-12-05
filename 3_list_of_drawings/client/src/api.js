0b867a014b5f654fa25f1aff3be798647ce6e75d
Ashif Zafar <ashif.mohammad@cdk.com>
2019-12-05 19:57:59 +0530

    Added example 2_3 with RethinkDb- fixed the on event

--- 3_list_of_drawings/client/src/api.js
+++ 3_list_of_drawings/client/src/api.js
@@ -3,7 +3,7 @@ const socket = openSocket('http://localhost:8000');
 
 export const subscribeToDrawings=(cb)=> {
   socket.on('drawing', cb);
-  socket.emit('subscribeToTimer', 1000);
+  socket.emit('subscribeToDrawing', 1000);
 }
 
 export const createDrawing = name => { console.log(name)
